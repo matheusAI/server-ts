3a98688971a0aac7b4e01301ead66d6c13496db4
Matheus Santos <ms2279584@gmail.com>
2024-03-19 12:49:01 -0300

    Atualizar o userController.ts

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from "express";
+import * as fs from 'fs';
 
-const users = [
-    {nome: "matheus",idader: 21},
-];
+const dataPath = 'src/data/usuarios.json'; // Caminho para o arquivo de dados dos usuários
 
 export default {
-    async index(req:Request, res:Response){
-        return res.json(users)
+    async index(req: Request, res: Response) {
+        try {
+            const rawData = fs.readFileSync(dataPath, 'utf8');
+            const users = JSON.parse(rawData);
+            return res.json(users);
+        } catch (error) {
+            console.error('Erro ao ler os dados dos usuários:', error);
+            return res.status(500).json({ error: 'Erro interno do servidor' });
+        }
     }
 }
