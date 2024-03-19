aa3a449284b49cebdfc993c308320cdb5aeb9281
matheusAI <ms2279584@gmail.com>
2024-03-19 14:40:42 -0300

    add types node

--- src/data/db.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-await mongoose.connect("mongodb://127.0.0.1/users",{})
-.then(() => {
-    console.log("mongodb conectado com sucesso")
-}).catch((err) => {
-    console.log("houve um error ao se conectar com mongodb: "+ err)
-});
