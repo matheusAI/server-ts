202b14f02fddc0239f13a2f7937411007a1e004b
matheusAI <ms2279584@gmail.com>
2024-09-03 20:01:41 -0300

    FIX: data_base

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,17 +3,19 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI as string;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is not defined in .env file");
+}
 
 export const conectarDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    } as mongoose.ConnectOptions);
+    await mongoose.connect(MONGODB_URI);
     console.log('Conectado ao MongoDB');
   } catch (error) {
     console.error('Erro ao conectar ao MongoDB:', error);
     process.exit(1);
   }
 };
+
